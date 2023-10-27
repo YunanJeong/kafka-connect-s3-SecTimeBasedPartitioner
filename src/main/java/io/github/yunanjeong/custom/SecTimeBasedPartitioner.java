90b0155ffcb1a0c77056f4735ff676cc5ab124fa
yunanjeong <njhgle@gmail.com>
2023-10-27 14:09:51 +0900

    indent standard 4

--- src/main/java/io/github/yunanjeong/custom/SecTimeBasedPartitioner.java
+++ src/main/java/io/github/yunanjeong/custom/SecTimeBasedPartitioner.java
@@ -1,5 +1,6 @@
 package io.github.yunanjeong.custom;
 
+
 import org.apache.kafka.connect.sink.SinkRecord;
 import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormatter;
@@ -20,24 +21,24 @@ public class SecTimeBasedPartitioner<T> extends TimeBasedPartitioner<T> {
     // 핵심 변경 로직
     @Override
     public String encodePartition(SinkRecord sinkRecord) {
-      Long timestamp = timestampExtractor.extract(sinkRecord);
-      timestamp = timestamp * 1000L;
-      return encodedPartitionForTimestamp(sinkRecord, timestamp);
+        Long timestamp = timestampExtractor.extract(sinkRecord);
+        timestamp = timestamp * 1000L;
+        return encodedPartitionForTimestamp(sinkRecord, timestamp);
     }
    
     // 상위 클래스와 동일한 메소드 (private이라 오버라이드 불가)
     private String encodedPartitionForTimestamp(SinkRecord sinkRecord, Long timestamp) {
-    if (timestamp == null) {
-      String msg = "Unable to determine timestamp using timestamp.extractor "
-          + timestampExtractor.getClass().getName()
-          + " for record: "
-          + sinkRecord;
-      log.error(msg);
-      throw new PartitionException(msg);
+        if (timestamp == null) {
+            String msg = "Unable to determine timestamp using timestamp.extractor "
+                + timestampExtractor.getClass().getName()
+                + " for record: "
+                + sinkRecord;
+            log.error(msg);
+            throw new PartitionException(msg);
+        }
+        DateTime bucket = new DateTime(
+            getPartition(partitionDurationMs, timestamp, formatter.getZone())
+        );
+        return bucket.toString(formatter);
     }
-    DateTime bucket = new DateTime(
-        getPartition(partitionDurationMs, timestamp, formatter.getZone())
-    );
-    return bucket.toString(formatter);
-  }
 }
