1e1d1731748070bba690b1ebde642309865be4f2
yunanjeong <njhgle@gmail.com>
2023-10-27 14:18:20 +0900

    커스텀 파티셔너가 선택되도록 설정

--- src/main/java/io/github/yunanjeong/custom/SecTimeBasedPartitioner.java
+++ src/main/java/io/github/yunanjeong/custom/SecTimeBasedPartitioner.java
@@ -1,14 +1,17 @@
 package io.github.yunanjeong.custom;
 
 
+import org.apache.kafka.common.config.ConfigException;
 import org.apache.kafka.connect.sink.SinkRecord;
 import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormatter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.apache.kafka.connect.errors.ConnectException;
 
 import io.confluent.connect.storage.errors.PartitionException;
 import io.confluent.connect.storage.partitioner.TimeBasedPartitioner;
+import io.confluent.connect.storage.partitioner.TimestampExtractor;
 
 public class SecTimeBasedPartitioner<T> extends TimeBasedPartitioner<T> {
     
@@ -18,14 +21,13 @@ public class SecTimeBasedPartitioner<T> extends TimeBasedPartitioner<T> {
     private long partitionDurationMs;
     private DateTimeFormatter formatter;
     
-    // 핵심 변경 로직
     @Override
     public String encodePartition(SinkRecord sinkRecord) {
         Long timestamp = timestampExtractor.extract(sinkRecord);
         timestamp = timestamp * 1000L;
         return encodedPartitionForTimestamp(sinkRecord, timestamp);
     }
-   
+    
     // 상위 클래스와 동일한 메소드 (private이라 오버라이드 불가)
     private String encodedPartitionForTimestamp(SinkRecord sinkRecord, Long timestamp) {
         if (timestamp == null) {
@@ -40,5 +42,37 @@ public class SecTimeBasedPartitioner<T> extends TimeBasedPartitioner<T> {
             getPartition(partitionDurationMs, timestamp, formatter.getZone())
         );
         return bucket.toString(formatter);
+    }
+
+    @Override
+    public TimestampExtractor newTimestampExtractor(String extractorClassName) {
+        try {
+            switch (extractorClassName) {
+                case "Wallclock":
+                case "Record":
+                case "RecordField":
+                    extractorClassName = "io.github.yunanjeong.custom.SecTimeBasedPartitioner$"
+                        + extractorClassName
+                        + "TimestampExtractor";
+                    break;
+                default:
+            }
+            Class<?> klass = Class.forName(extractorClassName);
+            if (!TimestampExtractor.class.isAssignableFrom(klass)) {
+                throw new ConnectException(
+                    "Class " + extractorClassName + " does not implement TimestampExtractor"
+                );
+            }
+            return (TimestampExtractor) klass.newInstance();
+        } catch (ClassNotFoundException
+                | ClassCastException
+                | IllegalAccessException
+                | InstantiationException e) {
+            ConfigException ce = new ConfigException(
+                "Invalid timestamp extractor: " + extractorClassName
+            );
+            ce.initCause(e);
+            throw ce;
+        }
     }
 }
